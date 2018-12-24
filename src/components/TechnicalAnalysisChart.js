877d955f11babe239e605cc15585509d8a200461
Hien Thi Nguyen <hientnguyen@C02MH85HFH00-4.local>
2018-12-24 19:58:36 +0700

    Update TechnicalAnalysisChart file

--- src/components/TechnicalAnalysisChart.js
+++ src/components/TechnicalAnalysisChart.js
@@ -7,7 +7,7 @@ export default class TechnicalAnalysisChart extends Component {
 		script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-technical-analysis.js';
 		script.innerHTML = JSON.stringify({
 			width: '100%',
-			height: 410,
+			height: '100%',
 			symbol: 'HOSE:VIC',
 			locale: 'en',
 			interval: '1D',
