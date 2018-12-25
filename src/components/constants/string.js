74376eace874e13b0b0b8c818b679bd6053f1b99
trungnguyen <quoctrunguit@gmail.com>
2018-12-25 16:02:02 +0700

    add error strings

--- src/components/constants/string.js
+++ src/components/constants/string.js
@@ -1,12 +1,21 @@
 const INVALID_TICKER = 'Ticker is invalid'
 const PRIOR_INVALID = 'Prior must be between 0 and 1'
+const START_DATE_INVALID = "Date that prediction starts must be before the max date of stock index"
+const TRAINING_YEARS_INVALID = "Training years must be within range"
+const MA_INVALID = "Moving Average must be greater than 0"
 
 export {
     INVALID_TICKER,
-    PRIOR_INVALID
+    PRIOR_INVALID,
+    START_DATE_INVALID,
+    TRAINING_YEARS_INVALID,
+    MA_INVALID
 }
 
 export default {
     INVALID_TICKER,
-    PRIOR_INVALID
+    PRIOR_INVALID,
+    START_DATE_INVALID,
+    TRAINING_YEARS_INVALID,
+    MA_INVALID
 }
