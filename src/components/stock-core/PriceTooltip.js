6938d313792d2566903e982f20479f84a08fa305
trungnguyen <quoctrunguit@gmail.com>
2018-12-25 16:01:29 +0700

    update prediction tool tip

--- src/components/stock-core/PriceTooltip.js
+++ src/components/stock-core/PriceTooltip.js
@@ -3,6 +3,8 @@ import { withStyles } from '@material-ui/core/styles';
 import CustomTooltip from '../common-core/CustomTooltip'
 import Typography from '@material-ui/core/Typography';
 
+var moment = require('moment')
+
 const styles= theme => ({
     tooltipContainer:{
         display: 'flex',
@@ -17,19 +19,22 @@ class PredictionTooltip extends CustomTooltip{
         if (active) {
         const { payload, label } = this.props;
         // let activeData = this.getDataFromLabel(label)
-        // let formattedLabel = moment(label).format('MMMM Do Y')
+        let formattedLabel = moment(label).format('MMM DD YYYY')
         return (
-                payload.map(data=>{
-                    const {dataKey, payload} = data
-                   
-                    return(
-                        <div key={dataKey} className={classes.tooltipContainer}>
-                            <Typography style={{fontWeight: 'bold'}} variant="body2">{dataKey.toUpperCase() + ": "}</Typography>
-                            &nbsp;
-                            <Typography variant="body2">{" " + payload[dataKey]}</Typography>
-                        </div>
-                    )
-                })
+               <div>
+                   <Typography>{formattedLabel}</Typography>
+                   { payload.map(data=>{
+                        const {dataKey, payload} = data
+                    
+                        return(
+                            <div key={dataKey} className={classes.tooltipContainer}>
+                                <Typography style={{fontWeight: 'bold'}} variant="body2">{dataKey.toUpperCase() + ": "}</Typography>
+                                &nbsp;
+                                <Typography variant="body2">{" " + payload[dataKey]}</Typography>
+                            </div>
+                        )
+                    })}
+               </div>
             );
         }
     
